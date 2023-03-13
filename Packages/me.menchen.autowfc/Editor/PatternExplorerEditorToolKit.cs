1ac71813f87d17503345ad6767d6633346fd4db8
Menchen <huangmenchen@gmail.com>
2023-03-13 02:22:53 +0100

    Add UI Toolkit drag^&drop

--- Packages/me.menchen.autowfc/Editor/PatternExplorerEditorToolKit.cs
+++ Packages/me.menchen.autowfc/Editor/PatternExplorerEditorToolKit.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using AutoWfc.Editor.UIToolKit;
 using AutoWfc.Wfc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.Tilemaps;
 using UnityEngine.UIElements;
 
 namespace AutoWfc.Editor
@@ -15,40 +19,121 @@ namespace AutoWfc.Editor
         [MenuItem("Tools/PatternToolKit")]
         public static void Init()
         {
-            Uikit = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Packages/me.menchen.autowfc/Editor/UIToolKit/PatternExplorer.uxml");
             EditorWindow wdn = GetWindow<PatternExplorerEditorToolKit>();
             wdn.titleContent = new GUIContent("ToolKit Pattern");
+            wdn.Show(true);
         }
 
-        public static VisualTreeAsset Uikit;
+        private VisualTreeAsset _baseUiTree;
 
+        private Dictionary<string, Sprite> _spriteLookUp = new();
         private List<WfcUtils<string>.Pattern> _patterns;
+
+        private Button _previewUp;
+        private Button _previewDown;
+        private Button _previewLeft;
+        private Button _previewRight;
+        private Button _previewCenter;
         public void CreateGUI()
         {
             rootVisualElement.Clear();
-            _cachedJson = PatternExplorerWindow.Current?.serializedJson;
-            if (string.IsNullOrEmpty(_cachedJson))
+            if (PatternExplorerWindow.Current == null )
             {
-                rootVisualElement.Add(new Label("Missing"));
+                var flex = new VisualElement()
+                {
+                    style =
+                    {
+                        flexDirection = new StyleEnum<FlexDirection>(FlexDirection.Column),
+                        alignItems = new StyleEnum<Align>(Align.Center),
+                        alignContent = new StyleEnum<Align>(Align.Center),
+                    }
+                };
+                
+                flex.Add(new Label("Missing"));
+                flex.Add(new Button(CreateGUI){text = "Refresh"});
+                rootVisualElement.Add(flex);
                 return;
             }
-            
+            _cachedJson = PatternExplorerWindow.Current.serializedJson;
+
+            _baseUiTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Packages/me.menchen.autowfc/Editor/UIToolKit/PatternExplorer.uxml");
             _patterns = JsonConvert.DeserializeObject<JObject>(_cachedJson)["Patterns"]
                 ?.ToObject<List<WfcUtils<string>.Pattern>>();
-            var root = Uikit.Instantiate();
+            var root = _baseUiTree.Instantiate();
             rootVisualElement.Add(root);
-            var patternList = root.Q("#PatternList");
-            patternList.Clear();
-            _patterns?.ForEach(e=>patternList.Add(new Button(){text = e.Value}));
+            _previewUp= root.Q<Button>("UP");
+            _previewLeft= root.Q<Button>("LEFT");
+            _previewRight= root.Q<Button>("RIGHT");
+            _previewDown= root.Q<Button>("DOWN");
+            _previewCenter= root.Q<Button>("MIDDLE");
+            var ghost = root.Q("Ghost");
+            var scroll = root.Q<ScrollView>("SCROLL");
+            UpdatePreviewButtons("Click to show neighbours");
+            GeneratePatternList(root, scroll, ghost);
         }
 
+        private void GeneratePatternList(TemplateContainer root, ScrollView scroll, VisualElement ghost)
+        {
+            var patternList = root.Q("PatternList");
+            patternList.Clear();
+            // var dragAndDropManipulator = new DragAndDropManipulator(root.Q("Ghost"),rootVisualElement);
+            _patterns?.ForEach(e =>
+            {
+                var obj = new VisualElement();
+                obj.style.backgroundImage = new StyleBackground(GetTileSprite(e.Value));
+                obj.style.height = 64;
+                obj.style.width = 64;
+                obj.style.marginBottom = 8;
+                obj.style.marginLeft = 8;
+                obj.style.marginRight = 8;
+                obj.style.marginTop = 8;
+                obj.tooltip = e.Value;
+                // obj.style.position = new StyleEnum<Position>(Position.Absolute);
+                // var bnt = new Button(() => PatternButtonClicked(e.Value)) { text = e.Value };
+                // obj.Add(bnt);
+                var dragAndDropManipulator = new DragAndDropManipulator(obj, scroll, ghost);
+                dragAndDropManipulator.OnStartDrop += () => UpdatePreviewButtons("Drop here to insert new pattern");
+                dragAndDropManipulator.OnEndDrop += () => UpdatePreviewButtons("Click to show neighbours");
+                patternList.Add(obj);
+            });
+        }
+        
+
+        private void UpdatePreviewButtons(string text)
+        {
+            // _previewCenter.style.backgroundImage = new StyleBackground(GetTileSprite(_patterns.Last().Value));
+            _previewDown.text = text;
+            _previewLeft.text = text;
+            _previewRight.text = text;
+            _previewUp.text = text;
+
+        }
+
+        private void PatternButtonClicked(string hash)
+        {
+            Debug.Log(hash);
+        }
+
+        protected Sprite GetTileSprite(string hash)
+        {
+            if (_spriteLookUp.TryGetValue(hash, out var texture))
+            {
+                return texture;
+            }
+
+            texture = Resources.Load<Tile>(hash).sprite;
+            // texture.filterMode = FilterMode.Point;
+            // ((Texture2D)texture).
+            _spriteLookUp[hash] = texture;
+            return texture;
+        }
         private string _cachedJson;
 
         public void OnGUI()
         {
-            if (PatternExplorerWindow.Current?.serializedJson != _cachedJson)
+            if (PatternExplorerWindow.Current.serializedJson != _cachedJson)
             {
-                _cachedJson = PatternExplorerWindow.Current?.serializedJson;
+                _cachedJson = PatternExplorerWindow.Current.serializedJson;
                 CreateGUI();
             }
         }
---
+++ Packages/me.menchen.autowfc/Editor/UIToolKit/DragAndDropManipulator.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using UnityEditor;
+
+namespace AutoWfc.Editor.UIToolKit
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.UIElements;
+
+    public class DragAndDropManipulator : PointerManipulator
+    {
+        public event Action OnStartDrop; 
+        public event Action OnDrag; 
+        public event Action OnEndDrop;
+        public event Action<VisualElement> OnDropped; 
+        // Write a constructor to set target and store a reference to the 
+        // root of the visual tree.
+        public DragAndDropManipulator(VisualElement target, ScrollView root, VisualElement ghost)
+        {
+            this.target = target;
+            this.root = root;
+            this.ghost = ghost;
+        }
+
+        protected override void RegisterCallbacksOnTarget()
+        {
+            // Register the four callbacks on target.
+            target.RegisterCallback<PointerDownEvent>(PointerDownHandler);
+            target.RegisterCallback<PointerMoveEvent>(PointerMoveHandler);
+            target.RegisterCallback<PointerUpEvent>(PointerUpHandler);
+            target.RegisterCallback<PointerCaptureOutEvent>(PointerCaptureOutHandler);
+        }
+
+        protected override void UnregisterCallbacksFromTarget()
+        {
+            // Un-register the four callbacks from target.
+            target.UnregisterCallback<PointerDownEvent>(PointerDownHandler);
+            target.UnregisterCallback<PointerMoveEvent>(PointerMoveHandler);
+            target.UnregisterCallback<PointerUpEvent>(PointerUpHandler);
+            target.UnregisterCallback<PointerCaptureOutEvent>(PointerCaptureOutHandler);
+        }
+
+        private Vector2 targetStartPosition { get; set; }
+
+        private Vector2 pointerStartPosition { get; set; }
+
+        private bool enabled { get; set; }
+
+        private ScrollView root { get; }
+
+        private VisualElement ghost { get; }
+
+        // This method stores the starting position of target and the pointer, 
+        // makes target capture the pointer, and denotes that a drag is now in progress.
+        private void PointerDownHandler(PointerDownEvent evt)
+        {
+            targetStartPosition = target.worldBound.position;
+            pointerStartPosition = evt.position;
+            target.CapturePointer(evt.pointerId);
+            enabled = true;
+            ghost.style.visibility = new StyleEnum<Visibility>(Visibility.Visible);
+            ghost.style.backgroundImage = target.style.backgroundImage;
+
+            var pos = (Vector2)evt.position - (pointerStartPosition - targetStartPosition) + root.scrollOffset;
+            pos = ghost.parent.WorldToLocal(pos);
+            ghost.transform.position = pos;
+            OnStartDrop?.Invoke();
+        }
+
+        // This method checks whether a drag is in progress and whether target has captured the pointer. 
+        // If both are true, calculates a new position for target within the bounds of the window.
+        private void PointerMoveHandler(PointerMoveEvent evt)
+        {
+            if (enabled && target.HasPointerCapture(evt.pointerId))
+            {
+                var pos = (Vector2)evt.position - (pointerStartPosition - targetStartPosition) + root.scrollOffset;
+                pos = ghost.parent.WorldToLocal(pos);
+                ghost.transform.position = pos;
+                OnDrag?.Invoke();
+            }
+        }
+
+        // This method checks whether a drag is in progress and whether target has captured the pointer. 
+        // If both are true, makes target release the pointer.
+        private void PointerUpHandler(PointerUpEvent evt)
+        {
+            if (enabled && target.HasPointerCapture(evt.pointerId))
+            {
+                target.ReleasePointer(evt.pointerId);
+                ghost.style.visibility = new StyleEnum<Visibility>(Visibility.Hidden);
+                OnEndDrop?.Invoke();
+            }
+        }
+
+        // This method checks whether a drag is in progress. If true, queries the root 
+        // of the visual tree to find all slots, decides which slot is the closest one 
+        // that overlaps target, and sets the position of target so that it rests on top 
+        // of that slot. Sets the position of target back to its original position 
+        // if there is no overlapping slot.
+        private void PointerCaptureOutHandler(PointerCaptureOutEvent evt)
+        {
+            if (enabled)
+            {
+                VisualElement slotsContainer = root.Q<VisualElement>(className: "slots");
+                UQueryBuilder<VisualElement> allSlots =
+                    slotsContainer.Query<VisualElement>(className: "slot");
+                UQueryBuilder<VisualElement> overlappingSlots =
+                    allSlots.Where(OverlapsTarget);
+                VisualElement closestOverlappingSlot =
+                    FindClosestSlot(overlappingSlots);
+                OnDropped?.Invoke(closestOverlappingSlot);
+                // Vector3 closestPos = Vector3.zero;
+                // if (closestOverlappingSlot != null)
+                // {
+                //     closestPos = RootSpaceOfSlot(closestOverlappingSlot);
+                //     closestPos = new Vector2(closestPos.x - 5, closestPos.y - 5);
+                // }
+
+                // target.transform.position =
+                //     closestOverlappingSlot != null ? closestPos : targetStartPosition;
+
+                enabled = false;
+            }
+        }
+
+        private bool OverlapsTarget(VisualElement slot)
+        {
+            return target.worldBound.Overlaps(slot.worldBound);
+        }
+
+        private VisualElement FindClosestSlot(UQueryBuilder<VisualElement> slots)
+        {
+            List<VisualElement> slotsList = slots.ToList();
+            float bestDistanceSq = float.MaxValue;
+            VisualElement closest = null;
+            foreach (VisualElement slot in slotsList)
+            {
+                Vector3 displacement =
+                    RootSpaceOfSlot(slot) - target.transform.position;
+                float distanceSq = displacement.sqrMagnitude;
+                if (distanceSq < bestDistanceSq)
+                {
+                    bestDistanceSq = distanceSq;
+                    closest = slot;
+                }
+            }
+
+            return closest;
+        }
+
+        private Vector3 RootSpaceOfSlot(VisualElement slot)
+        {
+            Vector2 slotWorldSpace = slot.parent.LocalToWorld(slot.layout.position);
+            return root.WorldToLocal(slotWorldSpace);
+        }
+    }
+}
