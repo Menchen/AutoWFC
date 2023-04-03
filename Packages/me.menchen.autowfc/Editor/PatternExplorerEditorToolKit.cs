13ebff8591e30725deb924cd2fdac935ff126eb6
Menchen <huangmenchen@gmail.com>
2023-04-03 20:14:50 +0200

    Pattern Explorer Part 2

--- Packages/me.menchen.autowfc/Editor/PatternExplorerEditorToolKit.cs
+++ Packages/me.menchen.autowfc/Editor/PatternExplorerEditorToolKit.cs
@@ -25,15 +25,23 @@ namespace AutoWfc.Editor
             wdn.titleContent = new GUIContent("ToolKit Pattern");
             wdn.Show(true);
         }
-        
-        public enum Direction
+
+        private enum Direction
         {
-            Left,Right,Up,Down
+            Left,
+            Right,
+            Up,
+            Down
         }
+
+        private readonly IReadOnlyCollection<Direction> _allDirection = new HashSet<Direction>()
+            { Direction.Left, Direction.Right, Direction.Up, Direction.Down };
 
         public static WfcHelper Current;
 
         private VisualTreeAsset _baseUiTree;
+
+        private HashSet<Direction> _filteredDirection = new();
 
         private Dictionary<string, Sprite> _spriteLookUp = new();
         private List<WfcUtils<string>.Pattern> _patterns;
@@ -51,6 +59,9 @@ namespace AutoWfc.Editor
         private TemplateContainer _root;
         private VisualElement _ghost;
         private ScrollView _scroll;
+
+        private VisualElement _prePreview;
+        private VisualElement _postPreview;
 
         public void CreateGUI()
         {
@@ -80,6 +91,11 @@ namespace AutoWfc.Editor
                     "Packages/me.menchen.autowfc/Editor/UIToolKit/PatternExplorer.uxml");
             _patterns = JsonConvert.DeserializeObject<JObject>(_cachedJson)["Patterns"]
                 ?.ToObject<List<WfcUtils<string>.Pattern>>();
+            if (_patterns!.All(e => e.Value != _centerValue))
+            {
+                _centerValue = null;
+            }
+
             _root = _baseUiTree.Instantiate();
             rootVisualElement.Add(_root);
             _previewUp = _root.Q<Button>("UP");
@@ -87,13 +103,14 @@ namespace AutoWfc.Editor
             _previewRight = _root.Q<Button>("RIGHT");
             _previewDown = _root.Q<Button>("DOWN");
             _previewCenter = _root.Q<Button>("MIDDLE");
+            _prePreview = _root.Q<VisualElement>("Pre-Preview");
+            _postPreview = _root.Q<VisualElement>("Post-Preview");
             _ghost = _root.Q("Ghost");
             _scroll = _root.Q<ScrollView>("SCROLL");
-            _whiteListPattern = _patterns!.Select(e => e.Value).ToHashSet();
             AddPreviewsButtonsHandler(NavigationButtonHandler);
-            UpdatePreviewButtons("Click to show neighbours");
+            UpdatePreviewButtonsText("Click to show neighbours");
             _previewCenter.text = "Click to show all pattern";
-            GeneratePatternList(_patterns, _root, _scroll, _ghost);
+            Refresh();
         }
 
         private void AddPreviewsButtonsHandler(Action<Button> handler)
@@ -106,6 +123,27 @@ namespace AutoWfc.Editor
         }
 
         private string _lastHoverText;
+
+        private void UpdatePreviewsButtons()
+        {
+            foreach (var direction in _allDirection)
+            {
+                var btn = GetButtonFromDirection(direction);
+                btn.style.backgroundColor =
+                    _filteredDirection.Contains(direction)
+                        ? new Color(184 / 255f, 88 / 255f, 88 / 255f, 1f)
+                        : new Color(88 / 255f, 88 / 255f, 88 / 255f, 1f);
+            }
+
+            if (_centerValue == null)
+            {
+                _previewCenter.style.backgroundImage = new StyleBackground();
+            }
+            else
+            {
+                _previewCenter.style.backgroundImage = new StyleBackground(GetTileSprite(_centerValue));
+            }
+        }
 
         private void GeneratePatternList(List<WfcUtils<string>.Pattern> patterns, TemplateContainer root,
             ScrollView scroll, VisualElement ghost)
@@ -132,13 +170,13 @@ namespace AutoWfc.Editor
                 // var bnt = new Button(() => PatternButtonClicked(e.Value)) { text = e.Value };
                 // obj.Add(bnt);
                 var dragAndDropManipulator = new DragAndDropManipulator(obj, scroll, ghost);
-                dragAndDropManipulator.OnStartDrop += () => UpdatePreviewButtons("Drop here to insert new pattern");
+                dragAndDropManipulator.OnStartDrop += () => UpdatePreviewButtonsText("Drop here to insert new pattern");
                 dragAndDropManipulator.OnStartDrop += () =>
                     obj.style.unityBackgroundImageTintColor =
                         isWhiteListed ? new Color(0.3f, 0.3f, 0.3f, 1f) : new Color(0.1f, 0.1f, 0.1f, 1);
                 dragAndDropManipulator.OnStartDrop += () => _previewCenter.text = "Drop here to switch pattern";
                 dragAndDropManipulator.OnEndDrop += () => _previewCenter.text = "Click to show all pattern";
-                dragAndDropManipulator.OnEndDrop += () => UpdatePreviewButtons("Click to show neighbours");
+                dragAndDropManipulator.OnEndDrop += () => UpdatePreviewButtonsText("Click to show neighbours");
                 dragAndDropManipulator.OnEndDrop += () =>
                     obj.style.unityBackgroundImageTintColor = defaultColor;
                 dragAndDropManipulator.OnDropped += element => DroppedHandler(e.Value, element);
@@ -167,30 +205,54 @@ namespace AutoWfc.Editor
         {
             if (slot.name == _previewCenter.name)
             {
-                UpdateCenterValue(pattern);
-                return;
+                _centerValue = pattern;
             }
-            // TODO Handle neighbours drop A.K.A Add to pattern
-            var dir = GetDirectionFromButtonName(slot.name);
-            AddPatternWithDir(_centerValue,dir,pattern);
-            
+            else
+            {
+                var dir = GetDirectionFromButtonName(slot.name);
+                TogglePatternWithDir(_centerValue, dir, pattern);
+            }
+
+            Refresh();
         }
 
-        private void AddPatternWithDir(string center, Direction dir, string pattern)
+        private void TogglePatternWithDir(string center, Direction dir, string pattern)
         {
             var centerPattern = _patterns.First(e => e.Value == center);
-            var patternId = _patterns.First(e => e.Value == pattern).Id;
-            centerPattern.Valid[(int)dir][patternId] = true;
+            var patternId = _patterns.First(e => e.Value == pattern);
+            var oldValue = centerPattern.Valid[(int)dir][patternId.Id];
+            centerPattern.Valid[(int)dir][patternId.Id] = !oldValue;
+            patternId.Valid[(int)GetComplementDirection(dir)][centerPattern.Id] = !oldValue;
             Save();
+        }
+
+        private Direction GetComplementDirection(Direction direction)
+        {
+            switch (direction)
+            {
+                case Direction.Left:
+                    return Direction.Right;
+                case Direction.Right:
+                    return Direction.Left;
+                case Direction.Up:
+                    return Direction.Down;
+                case Direction.Down:
+                    return Direction.Up;
+            }
+
+            throw new ArgumentException("Unknown Direction");
         }
 
         private void Save()
         {
             var jObj = JsonConvert.DeserializeObject<JObject>(_cachedJson);
             jObj["Patterns"] = JToken.FromObject(_patterns);
-            Undo.RecordObject(Current,"Pattern Explorer edit");
-            Current.serializedJson = jObj.ToString();
+            Undo.RecordObject(Current, "Pattern Explorer edit");
+            Current.serializedJson = jObj.ToString(Formatting.None);
+            _cachedJson = jObj.ToString();
             EditorUtility.SetDirty(Current);
+
+            Refresh();
         }
 
         private Button GetButtonFromDirection(Direction direction)
@@ -209,6 +271,7 @@ namespace AutoWfc.Editor
                     throw new ArgumentException("Unknown Direction");
             }
         }
+
         private Direction GetDirectionFromButtonName(string btnName)
         {
             if (btnName == _previewLeft.name)
@@ -225,6 +288,7 @@ namespace AutoWfc.Editor
             {
                 return Direction.Right;
             }
+
             if (btnName == _previewDown.name)
             {
                 return Direction.Down;
@@ -233,17 +297,8 @@ namespace AutoWfc.Editor
             throw new ArgumentException("Not preview button");
         }
 
-        private void UpdateCenterValue(string pattern)
-        {
-            _centerValue = pattern;
-            _previewCenter.style.backgroundImage = new StyleBackground(_spriteLookUp[pattern]);
-            var p = _patterns.First(e => e.Value == pattern);
-            PopulateWhiteList(p.Valid);
-            GeneratePatternList(_patterns, _root, _scroll, _ghost);
-        }
 
-
-        private void UpdatePreviewButtons(string text)
+        private void UpdatePreviewButtonsText(string text)
         {
             // _previewCenter.style.backgroundImage = new StyleBackground(GetTileSprite(_patterns.Last().Value));
             _previewDown.text = text;
@@ -297,6 +352,20 @@ namespace AutoWfc.Editor
                 .Select(e => _patterns.First(f => f.Id == e.Item2).Value).ToHashSet();
         }
 
+        private void UpdateWhiteList()
+        {
+            var center = CenterPattern;
+            if (string.IsNullOrEmpty(_centerValue) || center is null)
+            {
+                // Select All
+                _whiteListPattern = _patterns!.Select(e => e.Value).ToHashSet();
+            }
+            else
+            {
+                PopulateWhiteList(_allDirection.Except(_filteredDirection).Select(e => center.Valid[(int)e]).ToArray());
+            }
+        }
+
         private HashSet<string> _whiteListPattern = new();
 
         private void NavigationButtonHandler(Button btn)
@@ -310,35 +379,29 @@ namespace AutoWfc.Editor
             if (btn.name == _previewCenter.name)
             {
                 // Reset 
-                UpdateCenterValue(_centerValue);
-                return;
+                _filteredDirection.Clear();
+            }
+            else
+            {
+                var dir = GetDirectionFromButtonName(btn.name);
+                if (_filteredDirection.Contains(dir))
+                {
+                    _filteredDirection.Remove(dir);
+                }
+                else
+                {
+                    _filteredDirection.Add(dir);
+                }
             }
 
+            Refresh();
+        }
 
-            // LEFT
-            // RIGHT
-            // UP
-            // DOWN
-            if (btn.name == _previewLeft.name)
-            {
-                PopulateWhiteList(p.Valid[1], p.Valid[2], p.Valid[3]);
-                GeneratePatternList(_patterns, _root, _scroll, _ghost);
-            }
-            else if (btn.name == _previewUp.name)
-            {
-                PopulateWhiteList(p.Valid[1], p.Valid[0], p.Valid[3]);
-                GeneratePatternList(_patterns, _root, _scroll, _ghost);
-            }
-            else if (btn.name == _previewRight.name)
-            {
-                PopulateWhiteList(p.Valid[0], p.Valid[2], p.Valid[3]);
-                GeneratePatternList(_patterns, _root, _scroll, _ghost);
-            }
-            else if (btn.name == _previewDown.name)
-            {
-                PopulateWhiteList(p.Valid[1], p.Valid[2], p.Valid[0]);
-                GeneratePatternList(_patterns, _root, _scroll, _ghost);
-            }
+        private void Refresh()
+        {
+            UpdateWhiteList();
+            UpdatePreviewsButtons();
+            GeneratePatternList(_patterns, _root, _scroll, _ghost);
         }
 
         protected Sprite GetTileSprite(string hash)
